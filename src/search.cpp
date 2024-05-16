9208c5e0870f84721fbb85644d453ff38cab4afd
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2024-05-15 22:25:54 -0500

    I have no idea what I'm doing

--- src/search.cpp
+++ src/search.cpp
@@ -793,7 +793,11 @@ Value Search::Worker::search(
             ss->currentMove         = Move::null();
             ss->continuationHistory = &thisThread->continuationHistory[0][0][NO_PIECE][0];
 
+            pos.do_null_move(ss, tt);
+
             Value nullValue = -search<NonPV>(pos, ss + 1, -beta, -beta + 1, depth - R, !cutNode);
+
+            pos.undo_null_move();
 
             if (score >= beta)
             {
@@ -802,8 +806,6 @@ Value Search::Worker::search(
                 if (depth <= 0)
                     return qsearch<NonPV>(pos, ss, alpha - 1, alpha);
             }
-
-            pos.do_null_move;
         }
 
         else
