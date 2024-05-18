bf105665b8cb94f0ab6c74b59c9926f6e93d8aa2
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2024-05-17 23:07:38 -0500

    updated qsearch to be a full window

--- src/search.cpp
+++ src/search.cpp
@@ -804,7 +804,7 @@ Value Search::Worker::search(
                 depth -= 4;
 
                 if (depth <= 0)
-                    return qsearch<NonPV>(pos, ss, alpha - 1, alpha);
+                    return qsearch<NonPV>(pos, ss, alpha, beta);
             }
         }
 
