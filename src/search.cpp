9d9b542d1d407faeb1ba7bdd046cfb239261c909
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2024-06-05 21:06:22 -0500

    wider at lower depths

--- src/search.cpp
+++ src/search.cpp
@@ -315,7 +315,7 @@ void Search::Worker::iterative_deepening() {
             // Reset aspiration window starting size
             Value avg = rootMoves[pvIdx].averageScore;
 
-            delta = std::max(6 + 50 / rootDepth, 9) + avg * avg / 10502;
+            delta = std::max(6 + 50 / rootDepth, 25) + avg * avg / 10502;
             alpha = std::max(avg - delta, -VALUE_INFINITE);
             beta  = std::min(avg + delta, VALUE_INFINITE);
 
