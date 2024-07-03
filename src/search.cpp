8b8934bb47481595c6bdec087ed98fc7a1ffe619
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2024-07-03 12:26:58 -0500

    small margin
    bench 1214255

--- src/search.cpp
+++ src/search.cpp
@@ -1094,8 +1094,9 @@ moves_loop:  // When in check, search starts here
 
                 if (value < singularBeta)
                 {
-                    int doubleMargin = 290 * PvNode - 200 * !ttCapture;
-                    int tripleMargin = 107 + 247 * PvNode - 278 * !ttCapture + 99 * ss->ttPv;
+                    int doubleMargin = 290 * PvNode - 200 * !ttCapture - 50 * improving;
+                    int tripleMargin =
+                      107 + 247 * PvNode - 278 * !ttCapture + 99 * ss->ttPv - 50 * improving;
 
                     extension = 1 + (value < singularBeta - doubleMargin)
                               + (value < singularBeta - tripleMargin);
