df9595b71ac87db252eb99d576df9cf824e95350
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2024-08-01 10:12:28 +0800

    reduce futilitcy move count if we're in a cutnode
    bench 1230916

--- src/search.cpp
+++ src/search.cpp
@@ -74,8 +74,8 @@ Value futility_margin(Depth d, bool noTtCutNode, bool improving, bool oppWorseni
     return futilityMult * d - improvingDeduction - worseningDeduction;
 }
 
-constexpr int futility_move_count(bool improving, Depth depth) {
-    return (3 + depth * depth) / (2 - improving);
+constexpr int futility_move_count(bool improving, bool cutNode, Depth depth) {
+    return (3 + depth * depth) / (2 - improving) - cutNode;
 }
 
 // Add correctionHistory value to raw staticEval and guarantee evaluation
@@ -958,7 +958,7 @@ moves_loop:  // When in check, search starts here
         if (!rootNode && pos.non_pawn_material(us) && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
         {
             // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~8 Elo)
-            moveCountPruning = moveCount >= futility_move_count(improving, depth);
+            moveCountPruning = moveCount >= futility_move_count(improving, cutNode, depth);
 
             // Reduced depth of the next LMR search
             int lmrDepth = newDepth - r;
