457a51eb17a9045f1fb2ff8b6001819fd67a4fb6
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2024-08-08 09:15:02 +0800

    huh why are we updating corrhist in se
    bench 1329332

--- src/search.cpp
+++ src/search.cpp
@@ -1381,7 +1381,7 @@ moves_loop:  // When in check, search starts here
                        depth, bestMove, unadjustedStaticEval, tt.generation());
 
     // Adjust correction history
-    if (!ss->inCheck && (!bestMove || !pos.capture(bestMove))
+    if (!ss->inCheck && !excludedMove && (!bestMove || !pos.capture(bestMove))
         && !(bestValue >= beta && bestValue <= ss->staticEval)
         && !(!bestMove && bestValue >= ss->staticEval))
     {
