b877af24e33638b354e96426d38f586f0ffb7ffa
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2024-08-20 14:57:56 -0500

    yolo
    bench 1650202

--- src/search.cpp
+++ src/search.cpp
@@ -1196,7 +1196,7 @@ moves_loop:  // When in check, search starts here
 
             // Note that if expected reduction is high, we reduce search depth by 1 here (~9 Elo)
             value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha,
-                                   newDepth - (!cutNode && (r > 3)), !cutNode);
+                                   newDepth - (cutNode && (r > 3)), !cutNode);
         }
 
         // For PV nodes only, do a full PV search on the first move or after a fail high,
