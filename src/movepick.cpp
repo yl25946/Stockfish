b7ec68790eeb52422be4801fd13a941ccf28c3cd
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2024-09-08 20:03:52 -0500

    this one is truly a shot in the dark
    bench 1747517

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -155,7 +155,7 @@ void MovePicker::score() {
             m.value = (*mainHistory)[pos.side_to_move()][m.from_to()];
             m.value += 2 * (*pawnHistory)[pawn_structure_index(pos)][pc][to];
             m.value += 2 * (*continuationHistory[0])[pc][to];
-            m.value += (*continuationHistory[1])[pc][to];
+            m.value += 2 * (*continuationHistory[1])[pc][to];
             m.value += (*continuationHistory[2])[pc][to] / 3;
             m.value += (*continuationHistory[3])[pc][to];
             m.value += (*continuationHistory[5])[pc][to];
