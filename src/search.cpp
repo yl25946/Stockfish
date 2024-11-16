24f55f454086fe170f80ab02a0624667869c9609
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2024-11-15 20:12:18 -0600

    take 2
    bench 840721

--- src/search.cpp
+++ src/search.cpp
@@ -458,8 +458,9 @@ void Search::Worker::iterative_deepening() {
             // If the bestMove is stable over several iterations, reduce time accordingly
             timeReduction    = lastBestMoveDepth + 8 < completedDepth ? 1.495 : 0.687;
             double reduction = (1.48 + mainThread->previousTimeReduction) / (2.17 * timeReduction);
-            double bestMoveInstability       = 1 + 1.88 * totBestMoveChanges / threads.size();
-            double refutationMoveInstability = 1 + 1.4 * totRefutationMoveChanges / threads.size();
+            double bestMoveInstability = 1 + 1.88 * totBestMoveChanges / threads.size();
+            double refutationMoveInstability =
+              0.9 + 1.1 * totRefutationMoveChanges / threads.size();
             double recapture = limits.capSq == rootMoves[0].pv[0].to_sq() ? 0.955 : 1.005;
 
             double totalTime = mainThread->tm.optimum() * fallingEval * reduction
