8427fea5ea7403e57534e6889f649eb0af1d3b5b
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2025-02-06 20:16:24 -0600

    clamp r in nmp
    bench 2885863

--- src/search.cpp
+++ src/search.cpp
@@ -832,7 +832,7 @@ Value Search::Worker::search(
         assert(eval - beta >= 0);
 
         // Null move dynamic reduction based on depth and eval
-        Depth R = std::min(int(eval - beta) / 237, 6) + depth / 3 + 5;
+        Depth R = std::min(std::min(int(eval - beta) / 237, 6) + depth / 3 + 5, depth - 1);
 
         ss->currentMove                   = Move::null();
         ss->continuationHistory           = &thisThread->continuationHistory[0][0][NO_PIECE][0];
