331dabb6273af16a29075103b45ebec326959193
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2025-02-25 17:21:37 -0600

    lower margin in cutnode
    bench 2061187

--- src/search.cpp
+++ src/search.cpp
@@ -846,7 +846,7 @@ Value Search::Worker::search(
 
     // Step 9. Null move search with verification search
     if (cutNode && (ss - 1)->currentMove != Move::null() && eval >= beta
-        && ss->staticEval >= beta - 21 * depth + 455 - 60 * improving && !excludedMove
+        && ss->staticEval >= beta - 21 * depth + 455 - 60 * improving - 20 * cutNode && !excludedMove
         && pos.non_pawn_material(us) && ss->ply >= thisThread->nmpMinPly && !is_loss(beta))
     {
         assert(eval - beta >= 0);
