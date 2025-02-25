57d53c15ec14fef31e486aa7ac70f8e045d35c97
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2025-02-24 18:52:13 -0600

    another potential idea
    bench 2107600

--- src/search.cpp
+++ src/search.cpp
@@ -890,7 +890,7 @@ Value Search::Worker::search(
     // Step 10. Internal iterative reductions
     // For PV nodes without a ttMove as well as for deep enough cutNodes, we decrease depth.
     // (* Scaler) Especially if they make IIR more aggressive.
-    if (((PvNode || cutNode) && depth >= 7 - 3 * PvNode) && !ttData.move)
+    if (((PvNode || cutNode || !improving) && depth >= 7 - 3 * PvNode) && !ttData.move)
         depth--;
 
     // Step 11. ProbCut
