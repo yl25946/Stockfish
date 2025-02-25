ec43496a5c60f3123db9d4dcf0d597fe68bf2547
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2025-02-25 17:14:27 -0600

    adding cutnode to probcut
    bench 2377089

--- src/search.cpp
+++ src/search.cpp
@@ -896,7 +896,7 @@ Value Search::Worker::search(
     // Step 11. ProbCut
     // If we have a good enough capture (or queen promotion) and a reduced search
     // returns a value much above beta, we can (almost) safely prune the previous move.
-    probCutBeta = beta + 187 - 55 * improving;
+    probCutBeta = beta + 187 - 55 * improving - 20 * cutNode;
     if (depth >= 3
         && !is_decisive(beta)
         // If value from transposition table is lower than probCutBeta, don't attempt
