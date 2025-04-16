d625fabccd2d8ac83f1841ccfa57010b5e67e092
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2025-04-16 00:39:26 -0500

    this was shawn_xu's idea
    bench 1904727

--- src/search.cpp
+++ src/search.cpp
@@ -79,7 +79,7 @@ Value futility_margin(Depth d, bool noTtCutNode, bool improving, bool oppWorseni
 }
 
 constexpr int futility_move_count(bool improving, Depth depth) {
-    return (3 + depth * depth) / (2 - improving);
+    return (2 + depth * depth) / (2 - improving);
 }
 
 int correction_value(const Worker& w, const Position& pos, const Stack* const ss) {
